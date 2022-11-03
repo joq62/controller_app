a36ddc44b5ef41304ccdfba31b11f577d4fda8e8
joq62 <joakim.leche@gmail.com>
2022-11-03 21:24:15 +0100

    created

---
+++ tests/all.erl
@@ -0,0 +1,64 @@
+%%% -------------------------------------------------------------------
+%%% @author  : Joq Erlang
+%%% @doc: : 
+%%% Created :
+%%% Node end point  
+%%% Creates and deletes Pods
+%%% 
+%%% API-kube: Interface 
+%%% Pod consits beams from all services, app and app and sup erl.
+%%% The setup of envs is
+%%% -------------------------------------------------------------------
+-module(all).   
+ 
+-export([start/0]).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+-define(Id_c1,"c1").
+
+
+		 
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+   
+    ok=setup(),
+
+  
+    io:format("Test OK !!! ~p~n",[?MODULE]),
+    init:stop(),
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+
+
+setup()->
+    io:format("Start ~p~n",[?FUNCTION_NAME]),
+    
+    ok=application:start(controller_app),
+    pong=controller:ping(),
+    pong=common:ping(),
+    pong=sd:ping(),
+    pong=nodelog:ping(),
+    
+    io:format("Stop OK !!! ~p~n",[?FUNCTION_NAME]),
+
+    ok.
